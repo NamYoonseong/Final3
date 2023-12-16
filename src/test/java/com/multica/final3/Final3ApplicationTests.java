de5ce83d5feae1160fa252c359246d8fcc0e475b
yoon <ska5806@naver.com>
2023-12-16 20:19:31 +0900

    Initial commit

---
+++ src/test/java/com/multica/final3/Final3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multica.final3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Final3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
